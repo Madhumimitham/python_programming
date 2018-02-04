435ea6719a9e47ad1f8a88a9cc994972ebe29251
Madhumimitham <36121143+Madhumimitham@users.noreply.github.com>
2018-02-04 10:58:50 +0530

    Add files via upload

---
+++ beginner/slicing.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+finishers=['sam','bob','ada','bea']
+first_two=finishers[:2]
+print(first_two)
+copy_of_finishers=finishers[:]
+print(copy_of_finishers)
