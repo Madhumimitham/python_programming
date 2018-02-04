75fea3d12b28973e8b57c2cfab266a6f91958dac
Madhumimitham <36121143+Madhumimitham@users.noreply.github.com>
2018-02-04 12:19:35 +0530

    Add files via upload

---
+++ beginner/prompting_numerical_input.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+age=input("how old are you?")
+age =int(age)
+print(age)
+pi=input("what's the value of pi")
+pi=float(pi)
+print(pi)
