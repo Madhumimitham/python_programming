5b667260c557ceee3caa3da10e3af1b67b142f52
Madhumimitham <36121143+Madhumimitham@users.noreply.github.com>
2018-02-04 14:56:11 +0530

    Add files via upload

---
+++ players/string_op.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module5
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+string='Programmer'
+print(string.count('r'))
+print(string.find('g'))
+print(string.replace('m','y'))
