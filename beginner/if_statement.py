620cb11048e6515a60b5c1857f6148468c4e7f58
Madhumimitham <36121143+Madhumimitham@users.noreply.github.com>
2018-02-04 11:35:35 +0530

    Add files via upload

---
+++ beginner/if_statement.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+x=8
+if x ==42:
+   print("equal")
+y=10
+if y !=42:
+   print("not equal")
+z=5
+if z >42:
+   print("greater")
+p=48
+if p >=42:
+   print("greater than or equal")
+q=40
+if q <42:
+   print("less than ")
+r=10
+if r <=42:
+   print("less than or equal")
