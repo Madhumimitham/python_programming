eac179d0d27196a5833788c510b5311ec83d66a7
Madhumimitham <36121143+Madhumimitham@users.noreply.github.com>
2018-02-04 15:54:22 +0530

    Add files via upload

---
+++ players/sum_of_digit.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module7
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+x = sum(range(6))
+print(x)
