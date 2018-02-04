ae54e43f66454a6c9a6d0ab32b9bffd72c4002c5
Madhumimitham <36121143+Madhumimitham@users.noreply.github.com>
2018-02-04 10:42:22 +0530

    Add files via upload

---
+++ beginner/adding_item.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    bikes = ['hai','welcome']
+    print(bikes)
+    bikes.append('trek')
+    bikes.append('redline')
+    bikes.append('giant')
