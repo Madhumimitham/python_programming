be270dd2035f8041f78151603a2fb2bc556b87d5
Madhumimitham <36121143+Madhumimitham@users.noreply.github.com>
2018-02-04 15:38:58 +0530

    Add files via upload

---
+++ players/check_even_num.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module7
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+num=input("enter the num ber")
+def fun(k):
+    if k/2==0:
+       print("the given number is even")
+    else:
+        print("the given number is odd")
+fun(num)
