b3e4ed499a4ac5d6b678a7ed01a7fbb90de8cb54
Madhumimitham <36121143+Madhumimitham@users.noreply.github.com>
2018-02-04 10:46:47 +0530

    Add files via upload

---
+++ beginner/concat.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     03/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+first_name='albert'
+last_name='einstein'
+full_name=first_name+''+last_name
+print(full_name)
